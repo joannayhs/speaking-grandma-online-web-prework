9ad48862d3a7de6f21d6d82bd9179aea430b8c10
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-12 01:33:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if phrase == "Hi Nana, how are you?".downcase
+  if phrase == .downcase
     "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "WHAT DID YOU EAT TODAY?!".upcase
     "NO, NOT SINCE 1938!"
