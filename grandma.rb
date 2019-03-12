aa56a71986f36d911928b308094ff547ffca3cb1
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-12 01:38:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "#{phrase}".upcase
     "NO, NOT SINCE 1938!"
-    elsif phrase == "#{phrase}"
+  elsif phrase == "#{phrase}"
     "HUH?! SPEAK UP, SONNY!"
   end
 end
