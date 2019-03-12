67e9bc765e2392b0c176853b693f8cc561dee90a
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-12 01:25:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,12 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(phrase)
-if phrase == "I LOVE YOU GRANDMA!"
+speak_to_grandma(phrase)
+
+if phrase = "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "hello".upcase
-  puts "NO, NOT SINCE 1938"
-else 
+  elsif phrase = "Hi grandma, how are you?".downcase
   puts "HUH?! SPEAK UP, SONNY!"
-end
+  elsif phrase = ""
 
 
 
