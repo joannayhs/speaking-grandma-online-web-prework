ca9571def27db698ae9bff67f0da652fbe66226d
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-12 01:37:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,7 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
   elsif phrase == "#{phrase}".downcase
     "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == "#{phrase}".capitalize
   end
 end
 
